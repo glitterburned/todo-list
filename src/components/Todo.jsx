eba734b12092e65749d3cba205c8e24756fb4466
jennifer-louie <jenniferlouie@icloud.com>
2025-02-19 18:20:37 -0800

    Setup UI

---
+++ src/components/Todo.jsx
@@ -0,0 +1,26 @@
+import todo_icon from "../assets/todo_icon.png";
+import TodoItems from "./TodoItems";
+
+const Todo = () => {
+    return (
+        <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[500px] rounded-xl">
+            <div className="flex items-center mt-7 gap-2">
+                <img src={todo_icon} alt="To-Do icon" className="w-8" />
+                <h1 className="text-3xl font-semibold">To-Do List</h1>
+            </div>
+
+            <div className="flex items-center my-7 bg-gray-200 rounded-full">
+                <input type="text" placeholder="Add Task" className="bg-transparent border-0 outline-none 
+                    flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600" />
+                <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg 
+                    font-medium cursor-pointer">Add +</button>
+            </div>
+
+            <div>
+                <TodoItems />
+            </div>
+        </div>
+    )
+}
+
+export default Todo
