01882efbc1cf9a390f43763032bb5db5028515c6
jennifer-louie <jenniferlouie@icloud.com>
2025-02-21 09:09:08 -0800

    Add functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,53 @@
 import todo_icon from "../assets/todo_icon.png";
 import TodoItems from "./TodoItems";
+import { useRef, useState, useEffect } from "react";
 
 const Todo = () => {
+
+    const inputRef = useRef();
+    const [todoList, setTodoList] = useState(
+        localStorage.getItem("todoList") ? JSON.parse(localStorage.getItem("todoList")) : []);
+
+    // [add] button functionality
+    const add = () => {
+        const inputText = inputRef.current.value.trim();
+
+        if (inputText === "") {
+            return null;
+        }
+
+        const newTodo = {
+            id: Date.now(),
+            text: inputText,
+            isComplete: false
+        }
+        setTodoList((currentTodos) => [...currentTodos, newTodo]);
+        inputRef.current.value = "";
+    }
+
+    // [delete] button functionality
+    const deleteTodo = (id) => {
+        setTodoList((currentTodos) => {
+           return currentTodos.filter((item) => item.id !== id)
+        })
+    }
+
+    // check/uncheck functionality
+    const toggle = (id) => {
+        setTodoList((currentTodos) => {
+            return currentTodos.map((item) => {
+                if(item.id === id){
+                    return {...item, isComplete: !item.isComplete};
+                }
+                return item;
+            })
+        })
+    }
+    
+    useEffect(() => {
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+    }, [todoList])
+
     return (
         <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[500px] rounded-xl">
             <div className="flex items-center mt-7 gap-2">
@@ -10,14 +56,18 @@ const Todo = () => {
             </div>
 
             <div className="flex items-center my-7 bg-gray-200 rounded-full">
-                <input type="text" placeholder="Add Task" className="bg-transparent border-0 outline-none 
+                <input ref={inputRef} type="text" placeholder="Add Task" className="bg-transparent border-0 outline-none 
                     flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600" />
-                <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg 
+                <button 
+                    onClick={add}
+                    className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg 
                     font-medium cursor-pointer">Add +</button>
             </div>
 
             <div>
-                <TodoItems />
+                {todoList.map((item, index) => {
+                    return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}></TodoItems>
+                })}
             </div>
         </div>
     )
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,15 +1,17 @@
-import tick from "../assets/tick.png"
-import not_tick from "../assets/not_tick.png"
+import checked from "../assets/checked.png"
+import not_checked from "../assets/not_checked.png"
 import delete_icon from "../assets/delete.png"
 
-const TodoItems = () => {
+const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
     return (
         <div className="flex items-center my-3 gap-2">
-            <div className="flex flex-1 items-center cursor-pointer">
-                <img src={tick} alt="Check icon" className="w-7" />
-                <p className="text-slate-700 ml-4 text-[17px]"></p>
+            <div onClick={() => {toggle(id)}} className="flex flex-1 items-center cursor-pointer">
+                <img src={isComplete ? checked : not_checked} alt="Check icon" className="w-7" />
+                <p className={`text-slate-700 ml-4 text-[17px] ${isComplete ? "line-through" : ""}`}>
+                    {text}
+                </p>
             </div>
-            <img src={delete_icon} alt="Delete icon" className="w-3.5 cursor-pointer" />
+            <img onClick={() => {deleteTodo(id)}} src={delete_icon} alt="Delete icon" className="w-3.5 cursor-pointer" />
         </div>
     )
 }
